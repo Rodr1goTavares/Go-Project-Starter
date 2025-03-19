50eb19a412cc3e16ca88bf5956eeb542a440e906
Rodr1goTavares <rodr1gowzxz2@gmail.com>
2025-03-18 22:34:06 -0300

    :sparkles: initial commit.

---
+++ go-base.sh
@@ -0,0 +1,17 @@
+echo "Project name:"
+read projectName
+
+echo "Generating $projectName..."
+
+mkdir $projectName
+
+cd $projectName
+
+mkdir cmd internal pkg scripts
+mkdir cmd/$projectName
+
+echo "Folders created. Starting project module..."
+
+go mod init $projectName
+
+echo "Done."
